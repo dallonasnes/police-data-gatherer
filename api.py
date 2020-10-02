1cd5a0f197982cbd53b6d7bf006fe7e358453bf2
Dallon Asnes <dallon.asnes@gmail.com>
2020-10-02 16:52:09 -0500

    update procfile

--- api.py
+++ api.py
@@ -10,12 +10,12 @@ from data_aggregator import COPS_WITH_DATA_PICKLE_FILEPATH
 app = FastAPI()
 
 #read pickled data into memory and convert into json
-#with open(COPS_WITH_DATA_PICKLE_FILEPATH, "rb") as handle:
-#    cops_with_data: List[Cop] = pickle.load(handle)
+with open(COPS_WITH_DATA_PICKLE_FILEPATH, "rb") as handle:
+    cops_with_data: List[Cop] = pickle.load(handle)
 
 #singleton since once gathered data is static
 #but it may cause performance problems or heroku may not accept it on the free tier
-#cops_with_data_as_json = json.dumps([cop.get_dict() for cop in cops_with_data])
+cops_with_data_as_json = json.dumps([cop.get_dict() for cop in cops_with_data])
 
 @app.get('/')
 def read_root():
@@ -23,5 +23,4 @@ def read_root():
 
 @app.get('/cops')
 def cops():
-    return "can you see this?"
-    #return cops_with_data_as_json
+    return cops_with_data_as_json
